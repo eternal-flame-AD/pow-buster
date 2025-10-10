6d3f2dc94c253f3f805a53f337248b8c922997dd
eternal-flame-AD <yume@yumechi.jp>
2025-10-10 01:05:13 -0500

    refactor: retire Nollamas solver (resolves #2)
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>

--- src/solver/safe.rs
+++ src/solver/safe.rs
@@ -4,7 +4,7 @@ use crate::{
     Align16, Align64,
     message::{
         BinaryMessage, CerberusMessage, DecimalMessage, DoubleBlockMessage, GoAwayMessage,
-        GoToSocialMessage, SingleBlockMessage,
+        SingleBlockMessage,
     },
 };
 
@@ -509,151 +509,8 @@ impl crate::solver::Solver for CerberusSolver {
     }
 }
 
-/// Safe GoToSocial solver.
-///
-///
-/// Current implementation: generic sha2 crate fallback.
-pub struct GoToSocialSolver {
-    max_nonce: u64,
-    message: GoToSocialMessage,
-    attempted_nonces: u64,
-}
-
-impl From<GoToSocialMessage> for GoToSocialSolver {
-    fn from(message: GoToSocialMessage) -> Self {
-        Self {
-            max_nonce: Self::ABSOLUTE_MAX_NONCE,
-            message,
-            attempted_nonces: 0,
-        }
-    }
-}
-
-impl GoToSocialSolver {
-    const ABSOLUTE_MAX_NONCE: u64 = 10u64.pow(10) - 1;
-
-    /// Set the max nonce.
-    pub fn set_max_nonce(&mut self, max_nonce: u64) {
-        self.max_nonce = max_nonce;
-    }
-
-    /// Get the attempted nonces.
-    pub fn get_attempted_nonces(&self) -> u64 {
-        self.attempted_nonces
-    }
-
-    /// Extract the target from the image hex.
-    pub fn extract_target(image_hex: &[u8]) -> Option<u64> {
-        if image_hex.len() == 32 {
-            let d_bytes = &image_hex[(4 * 3)..(4 * 3 + 4)];
-            let h_bytes = &image_hex[(4 * 7)..(4 * 7 + 4)];
-            return Some(
-                (u32::from_be_bytes(d_bytes.try_into().unwrap()) as u64) << 32
-                    | u32::from_be_bytes(h_bytes.try_into().unwrap()) as u64,
-            );
-        }
-
-        if image_hex.len() != 64 {
-            return None;
-        }
-
-        let mut image_words = [0u32; 8];
-        for i in 0..8 {
-            let mut word = 0u32;
-            for j in 0..4 {
-                let high_nibble = if image_hex[i * 8 + j * 2].is_ascii_digit() {
-                    image_hex[i * 8 + j * 2] - b'0'
-                } else if (b'a'..=b'f').contains(&image_hex[i * 8 + j * 2]) {
-                    image_hex[i * 8 + j * 2] - b'a' + 10
-                } else {
-                    unreachable!();
-                };
-                let low_nibble = if image_hex[i * 8 + j * 2 + 1].is_ascii_digit() {
-                    image_hex[i * 8 + j * 2 + 1] - b'0'
-                } else if (b'a'..=b'f').contains(&image_hex[i * 8 + j * 2 + 1]) {
-                    image_hex[i * 8 + j * 2 + 1] - b'a' + 10
-                } else {
-                    unreachable!();
-                };
-                word <<= 4;
-                word |= high_nibble as u32;
-                word <<= 4;
-                word |= low_nibble as u32;
-            }
-            image_words[i] = word;
-        }
-        Some((image_words[3] as u64) << 32 | image_words[7] as u64)
-    }
-}
-
-impl crate::solver::Solver for GoToSocialSolver {
-    fn solve<const TYPE: u8>(&mut self, target: u64, _mask: u64) -> Option<(u64, [u32; 8])> {
-        const INDEX_D: usize = 3;
-        const INDEX_H: usize = 7;
-
-        let target_d = (target >> 32) as u32;
-        let target_h = target as u32;
-
-        for nonce in (0..=self.max_nonce).rev() {
-            let mut state = crate::sha256::IV;
-            let seed_words = self.message.as_words();
-            let mut blocks = [
-                seed_words[0],
-                seed_words[1],
-                seed_words[2],
-                seed_words[3],
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-            ];
-
-            let mut itoa_buffer = [0x80; 11];
-            let mut j = 10;
-            let mut nonce_copy = nonce;
-            loop {
-                j -= 1;
-                itoa_buffer[j] = (nonce_copy % 10) as u8 + b'0';
-                nonce_copy /= 10;
-                if nonce_copy == 0 {
-                    break;
-                }
-            }
-            let digits = &itoa_buffer[j..];
-            // pading 1 byte
-            let msg_len = (digits.len() as u32 + GoToSocialMessage::SEED_LEN - 1) * 8;
-            blocks[15] = msg_len;
-            for (i, digit) in digits.iter().enumerate() {
-                let message = crate::decompose_blocks_mut(&mut blocks);
-                let idx = 16 + i;
-                message[crate::SWAP_DWORD_BYTE_ORDER[idx]] = *digit;
-            }
-
-            crate::sha256::digest_block(&mut state, &blocks);
-
-            if state[INDEX_D] == target_d && state[INDEX_H] == target_h {
-                return Some((nonce, state));
-            }
-
-            self.attempted_nonces += 1;
-        }
-
-        None
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::solver::Solver;
-
     use super::*;
 
     #[test]
@@ -665,26 +522,6 @@ mod tests {
                 DoubleBlockMessage::new(prefix, search_space).map(Into::into)
             }
         });
-    }
-
-    #[test]
-    fn test_solve_go_to_social() {
-        let test_seed = *b"0f0314853c035207";
-        let known_solution = 498330;
-        let image = b"9c1351f7b40f2babe4e1b02c481ec94a2e14d91164da0925ca03845035891271";
-
-        let target = GoToSocialSolver::extract_target(image).unwrap();
-
-        let message = GoToSocialMessage::new(test_seed);
-        let mut solver = GoToSocialSolver::from(message);
-        solver.set_max_nonce(known_solution * 2);
-        let solution = solver
-            .solve::<{ crate::solver::SOLVE_TYPE_DH_PREIMAGE }>(target, u64::MAX)
-            .unwrap();
-        assert_eq!(solution.0, known_solution);
-        let mut solution_hex = [0u8; 64];
-        crate::encode_hex(&mut solution_hex, solution.1);
-        assert_eq!(&solution_hex, image);
     }
 
     #[test]
